d9bfe6b303b77511f6f07462a4472029717663be
BDinhDuy <dinhduy2672@gmail.com>
2025-12-20 19:56:30 +0700

    Initial commit: Vue 3 + Vite + Vuetify + Pinia setup

---
+++ src/stores/index.js
@@ -0,0 +1,6 @@
+// Export all stores from here for easy import
+export { useCounterStore } from './counter'
+
+// Example: Add more stores as needed
+// export { useUserStore } from './user'
+// export { useProductStore } from './product'
