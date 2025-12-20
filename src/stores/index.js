c17124c67a72a7b17542181cbf99574b5b2e2780
BDinhDuy <dinhduy2672@gmail.com>
2025-12-20 20:00:48 +0700

    test change

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,6 +1,1 @@
-// Export all stores from here for easy import
 export { useCounterStore } from './counter'
-
-// Example: Add more stores as needed
-// export { useUserStore } from './user'
-// export { useProductStore } from './product'
