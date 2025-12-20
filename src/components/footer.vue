1fafd7702d3a30b3090bc0c033163c6ee0ca75ba
BDinhDuy <dinhduy2672@gmail.com>
2025-12-21 02:42:54 +0700

    add header and footer

---
+++ src/components/footer.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    // component logic
+}
+</script>
+
+<style scoped>
+/* CSS styles */
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- HTML template của bạn -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentName',
+  // component logic
+}
+</script>
+
+<style scoped>
+/* CSS styles */
+</style>
